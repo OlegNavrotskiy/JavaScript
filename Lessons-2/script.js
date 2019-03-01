b00efb099abce16bff89809e2618fa9c7daf1e34
Oleg <olehmu93@gmail.com>
2019-03-01 18:58:17 +0200

    Изменил цикл в уроке 2

--- Lessons-2/script.js
+++ Lessons-2/script.js
@@ -11,38 +11,57 @@ let appData = {
       savings: false
   };
 
+//Способ №1  
 for (let i = 0; i < 2; i++) {
     let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
         b = prompt("Во сколько обойдется?", '');
+        if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) !=
+        null && a != '' && b != '' && a.length < 50) {
+        console.log("Done");
+        appData.expenses[a] = b;
+      } else {
+        console.log("Некоректная информация в ответах группы №" + i);
+        console.log(appData.expenses);
+        i--;
+      }
+    } 
 
-//Второй способ
-/*     let i = 0,
-      a, b;
-    while (i < 2) {
-      a = prompt("Введите обязательную статью расходов в этом месяце", '');
-      b = prompt("Во сколько обойдется?", '');
-      i++; */
+//Способ №2
+/* let i = 0,
+    a, b;
+while (i < 2) {
+    a = prompt("Введите обязательную статью расходов в этом месяце", '');
+    b = prompt("Во сколько обойдется?", '');
+    i++; 
+if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) !=
+    null && a != '' && b != '' && a.length < 50) {
+    console.log("Done");
+    appData.expenses[a] = b;
+} else {
+    console.log("Некоректная информация в ответах группы №" + i);
+    console.log(appData.expenses);
+    i--;
+  }
+}  */
     
-//Третий способ (цикл работает, но доп.условия проверки на ввод нужно изменять)
-/*     let i = 0,
-        a, b;
-    do {
-      a = prompt("Введите обязательную статью расходов в этом месяце", '');
-      b = prompt("Во сколько обойдется?", '');
-      i++;
-    }
-    while (i < 2); */
-
-    if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) !=
-      null && a != '' && b != '' && a.length < 50) {
-      console.log("Done");
-      appData.expenses[a] = b;
-    } else {
-      console.log("Некоректная информация в ответах группы №" + i);
-      console.log(appData.expenses);
-      i--;
-    }
-  } 
+//Способ №3
+/* let i = 0,
+    a, b;
+do {
+    a = prompt("Введите обязательную статью расходов в этом месяце", '');
+    b = prompt("Во сколько обойдется?", '');
+    i++;
+if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) !=
+    null && a != '' && b != '' && a.length < 50) {
+    console.log("Done");
+    appData.expenses[a] = b;
+} else {
+    console.log("Некоректная информация в ответах группы №" + i);
+    console.log(appData.expenses);
+    i--;
+  }
+} 
+while (i < 2); */
 
 appData.moneyPerDay = appData.budget / 30;
 alert("Ежедневный бюджет: " + appData.moneyPerDay);
