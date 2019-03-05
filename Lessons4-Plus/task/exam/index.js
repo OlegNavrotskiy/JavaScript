2c72ec9293ad9b24f2b2577c2ca37105b6bb3483
Oleg <olehmu93@gmail.com>
2019-03-05 17:13:55 +0200

    Урок 4 + доп. задание

--- Lessons4-Plus/task/exam/index.js
+++ Lessons4-Plus/task/exam/index.js
@@ -1,36 +1,41 @@
-'use strict';
-
 let friendlyNums = [];
 
 function getFriendlyNumbers(start, end) {
+// Проверка на правильность ввода аргументов функции
   if (
     typeof start == "number" &&
     typeof end == "number" &&
     1 <= start && start <= end &&
     parseInt(start) == start &&
     parseInt(end) == end ) {
+//Ищем два элемента
       for (let i = start; i <= end; i++) {
         for (let j = start; j <= i; j++) {
           if (
+//Проверяем через созданую функцию сумм делителей на повторение
             getDivisorsSum(j) === i &&
             getDivisorsSum(j) !== j &&
             getDivisorsSum(i) === j &&
             getDivisorsSum(i) !== i &&
             getDivisorsSum(i) > start) 
             {
+//Записываем массив с элементами в массив дружественных чисел
               friendlyNums.push([i, j].sort());
             }
           }
         }
       } else {
+//Возвращаем false если главные условие не выполняются
         return false;
       }
+//Выводим в консоль наш полученный массив
       console.log(friendlyNums);
       return friendlyNums;
     }
-getFriendlyNumbers(300, 500);
+//Проверка на работоспособность
+//    getFriendlyNumbers(1, 1210);
 
-
+//Массив делителей
 function getDivisors(num) {
   let arrDivisors = [];
   for (let i = 1; i < num; i++) {
@@ -41,6 +46,7 @@ function getDivisors(num) {
   return arrDivisors;
 }
 
+//Сумма элементов массива
 function getSum(arr) {
   let sum = 0;
   for (let i = 0; i < arr.length; i++) {
@@ -48,6 +54,14 @@ function getSum(arr) {
   }
   return sum;
 }
+
+//Сумма делителей
 function getDivisorsSum(num) {
   return getSum(getDivisors(num));
+}
+
+module.exports = {
+    firstName: 'Oleg',
+    secondName: 'Navrotskiy',
+    task: getFriendlyNumbers
 }
