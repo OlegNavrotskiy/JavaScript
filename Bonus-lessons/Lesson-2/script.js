c59137fa2ce33f9cd75358fe72f4137952af5846
Oleg <olehmu93@gmail.com>
2019-03-15 23:41:06 +0200

    Oh

--- Bonus-lessons/Lesson-2/script.js
+++ Bonus-lessons/Lesson-2/script.js
@@ -1,4 +1,5 @@
 let btnRus = document.getElementsByClassName('popup__call-wrap')[0],
+    btnSu = document.getElementsByClassName('popup__call-wrap')[1],
     hidBlock = document.getElementsByClassName('popup__call-phone')[1],
     block = document.getElementsByClassName('popup__call-phone')[0],
     ar = document.getElementsByClassName('popup__call-svg')[0],
@@ -7,11 +8,6 @@ let btnRus = document.getElementsByClassName('popup__call-wrap')[0],
     country = false;
 
 btnRus.addEventListener('click', function() {
-
-  
-  hidBlock.style.visibility = 'visible';
-  ar.style.transform = 'rotate(180deg)';
-
 
     if (visible) {
         hidBlock.style.visibility = 'hidden';
@@ -25,12 +21,15 @@ btnRus.addEventListener('click', function() {
 
 });
 
+
 hidBlock.addEventListener('click', function() {
   
   allBlock.insertBefore(hidBlock, block);
   hidBlock.classList.remove('popup__call-phone-hidden');
   block.classList.add('popup__call-phone-hidden');
-
+  block.style.visibility = 'hidden';
+  ar.style.transform = 'rotate(0deg)';
+  btnRus = 0;
 });
 
 
