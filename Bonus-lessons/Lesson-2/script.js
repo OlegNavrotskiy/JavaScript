9388e0ee8d4914270e3eac0d844b2ca47e1b8b20
Oleg <olehmu93@gmail.com>
2019-03-16 14:52:08 +0200

    Working

--- Bonus-lessons/Lesson-2/script.js
+++ Bonus-lessons/Lesson-2/script.js
@@ -1,35 +1,76 @@
-let btnRus = document.getElementsByClassName('popup__call-wrap')[0],
-    btnSu = document.getElementsByClassName('popup__call-wrap')[1],
-    hidBlock = document.getElementsByClassName('popup__call-phone')[1],
-    block = document.getElementsByClassName('popup__call-phone')[0],
-    ar = document.getElementsByClassName('popup__call-svg')[0],
+let btnRus = document.getElementsByClassName('popup__call-wrap'),
+    btnSu = document.getElementsByClassName('popup__call-wrap'),
+    hidBlock = document.getElementsByClassName('popup__call-phone'),
+    block = document.getElementsByClassName('popup__call-phone'),
+    ar = document.getElementsByClassName('popup__call-svg'),
     allBlock = document.querySelector('.popup__call-phoneblock'),
     visible = false,
-    country = false;
+    rus = true,
+    bel = false,
+    rusHid = false;
 
-btnRus.addEventListener('click', function() {
+btnRus[0].addEventListener('click', function() {
 
     if (visible) {
-        hidBlock.style.visibility = 'hidden';
-        ar.style.transform = 'rotate(0deg)';
+        hidBlock[1].style.visibility = 'hidden';
+        ar[0].style.transform = 'rotate(0deg)';
         visible = false;
     } else {
-      hidBlock.style.visibility = 'visible';
-      ar.style.transform = 'rotate(180deg)';
+      hidBlock[1].style.visibility = 'visible';
+      ar[0].style.transform = 'rotate(180deg)';
       visible = true;
+      rusHid = false;
+      btnSu[1].style.pointerEvents = 'none';
     }
 
 });
 
+btnSu[1].addEventListener('click', function() {
+  if (!rus && !rusHid) {
+hidBlock[1].style.visibility = 'visible';
+ar[0].style.transform = 'rotate(180deg)';
+rusHid = true;
+btnSu[1].style.pointerEvents = 'none';
+  } else {
+    hidBlock[1].style.visibility = 'hidden';
+    ar[0].style.transform = 'rotate(0deg)';
+    rusHid = false;
+    visible = false;
 
-hidBlock.addEventListener('click', function() {
-  
-  allBlock.insertBefore(hidBlock, block);
-  hidBlock.classList.remove('popup__call-phone-hidden');
-  block.classList.add('popup__call-phone-hidden');
-  block.style.visibility = 'hidden';
-  ar.style.transform = 'rotate(0deg)';
-  btnRus = 0;
+  }
 });
+
+
+
+hidBlock[1].addEventListener('click', function() {
+  
+if (rus) {
+  allBlock.insertBefore(hidBlock[1], block[0]);
+  hidBlock[0].classList.remove('popup__call-phone-hidden');
+  block[1].classList.add('popup__call-phone-hidden');
+  block[1].style.visibility = 'hidden';
+  ar[0].style.transform = 'rotate(0deg)';
+  rus = false;
+  bel = true;
+  btnSu[0].style.pointerEvents = '';
+} 
+});
+
+hidBlock[0].addEventListener('click', function() {
+  
+  if (bel) {
+allBlock.insertBefore(block[1],hidBlock[0]);
+block[0].classList.remove('popup__call-phone-hidden');
+hidBlock[1].classList.add('popup__call-phone-hidden');
+hidBlock[1].style.visibility = 'hidden';
+ar[0].style.transform = 'rotate(0deg)';
+rus = true;
+bel = false;
+btnSu[0].style.pointerEvents = '';
+  } 
+
+});
+
+
 
 
